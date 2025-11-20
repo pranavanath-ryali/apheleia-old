30c59640c0b177d403fc553fd12696deb7402c72
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-11-21 01:24:50 +0530

    use crossterm to print out a basic A red char

--- crates/apheleia-core/src/lib.rs
+++ crates/apheleia-core/src/lib.rs
@@ -1,3 +1,31 @@
+use std::io::{stdout, Write};
+use crossterm::{cursor, queue, style::{self, Stylize}, terminal::{self, Clear}, ExecutableCommand, QueueableCommand};
+
+pub fn setup() {
+    let size = terminal::size();
+    if let Ok(s) = size {
+        println!("{} {}", s.0, s.1);
+    }
+
+    let mut stdout = stdout();
+
+    stdout.execute(Clear(terminal::ClearType::All));
+
+    queue!(stdout,
+        cursor::MoveTo(0, 0),
+        style::PrintStyledContent("A".red())
+    );
+
+    // stdout
+    //     .queue(Clear(terminal::ClearType::All));
+    //
+    // queue!(stdout, cursor::MoveTo(5, 5));
+
+
+
+    stdout.flush();
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
--- examples/core-hello-world/src/main.rs
+++ examples/core-hello-world/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    apheleia_core::setup();
 }
