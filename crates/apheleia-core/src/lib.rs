2b99e038532b3a4aba89a410796aa7be3adb22a5
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-11-21 09:16:41 +0530

    screen filled with 2

--- crates/apheleia-core/src/lib.rs
+++ crates/apheleia-core/src/lib.rs
@@ -1,33 +1,51 @@
-use std::io::{stdout, Write};
-use crossterm::{cursor, queue, style::{self, Stylize}, terminal::{self, Clear}, ExecutableCommand, QueueableCommand};
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor, queue,
+    style::{self, Stylize},
+    terminal::{self, Clear},
+};
+use std::io::{Write, stdout};
 
 pub fn setup() {
     let size = terminal::size();
     if let Ok(s) = size {
         println!("{} {}", s.0, s.1);
+
+        let mut stdout = stdout();
+
+        stdout.execute(Clear(terminal::ClearType::All));
+
+        // queue!(
+        //     stdout,
+        //     cursor::MoveTo(3, 0),
+        //     style::PrintStyledContent("A".red())
+        // );
+        // queue!(
+        //     stdout,
+        //     cursor::MoveTo(5, 0),
+        //     style::PrintStyledContent("123".red())
+        // );
+        //
+        for i in 0..(s.0 - 0) {
+            // queue!(
+            //     stdout,
+            //     cursor::MoveTo(i, 0),
+            //     style::PrintStyledContent("2".red())
+            // );
+            for j in 0..(s.1 - 0) {
+                queue!(
+                    stdout,
+                    cursor::MoveTo(i, j),
+                    style::PrintStyledContent("2".red())
+                );
+            }
+        }
+
+        stdout.flush();
     }
-
-    let mut stdout = stdout();
-
-    stdout.execute(Clear(terminal::ClearType::All));
-
-    queue!(stdout,
-        cursor::MoveTo(0, 0),
-        style::PrintStyledContent("A".red())
-    );
-
     // stdout
     //     .queue(Clear(terminal::ClearType::All));
     //
     // queue!(stdout, cursor::MoveTo(5, 5));
-
-
-
-    stdout.flush();
-}
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
 }
 
 #[cfg(test)]
