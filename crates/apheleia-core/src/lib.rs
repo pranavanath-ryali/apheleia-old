88b44f3ca1bac92e6e201619273c232b0783ea55
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-11-23 01:25:56 +0530

    changed to type of index to u16 for buffers and renderer

--- crates/apheleia-core/src/lib.rs
+++ crates/apheleia-core/src/lib.rs
@@ -1,20 +1,31 @@
 pub mod buffer;
 pub mod renderer;
 
-use std::thread::{sleep, sleep_ms};
+use std::thread::sleep_ms;
 
 use crate::{buffer::Buffer, renderer::Renderer};
-use crossterm::
-    terminal::{self}
-;
+use crossterm::terminal::{self};
 
 pub fn setup() {
     let size = terminal::size().unwrap();
-    let mut buf = Buffer::new(size.0 as usize, size.1 as usize);
+    let mut buf = Buffer::new_fill(size.0 - 10, size.1 - 20, 'c');
     let mut renderer = Renderer::new();
 
     buf.write_line(0, 0, "+");
     buf.write_line(10, 3, "Hello World!");
 
     renderer.flip(&mut buf);
+    
+    let mut i: u16 = 1;
+    buf.write_line(0, 0, "HI");
+    loop {
+        sleep_ms(1000);
+        buf.write_line(i - 1, i - 1, "  ");
+        buf.write_line(i, i, "HI");
+
+        renderer.update(&mut buf);
+        i += 1;
+    }
+
+    renderer.quit();
 }
